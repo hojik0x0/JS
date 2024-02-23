2c03d1427375fccda872de845ed84f5866a19fe7
Hojik <rasulov0x0@gmail.com>
2024-02-24 00:35:04 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,10 @@
+var Alisher = 10000000;
+const Bilet = 500*11000.34;
+const Mehmonxona = 250*11000.34;
+const Muzey = 120*12354.03;
+let Xarajat = Bilet+Mehmonxona+Muzey;
+if (Alisher > Xarajat) {
+    console.log("Oq yol, Alisher!")
+} else{
+    console.log("Alisher, ozgina sabr qilish kerak bolar ekan")
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+1
+let Yosh = 14;
+// 1 yil
+Yosh++
+console.log(Yosh)
+
+2
+// 2024 yilda yoshi
+let yosh = 14;
+// 2023 yildagi yoshi
+yosh--
+console.log(yosh)
+
+3
+
+
+
+4
+
+
+
+5
+let meningYoshim = 14;
+let dostimningYoshi = ++meningYoshim;
+console.log(dostimningYoshi)
+
+6
+let MeningYoshim = 14;
+let singlimningYoshi = --MeningYoshim;
+console.log(singlimningYoshi)
+
+7
+let oldingiBaxoyim = 4;
+let hozirgiBaxoyim = oldingiBaxoyim + 1;
+console.log(hozirgiBaxoyim)
+
+8
+let OldingiBaxoyim = 5;
+let HozirgiBaxoyim = OldingiBaxoyim - 1;
+console.log(HozirgiBaxoyim)
